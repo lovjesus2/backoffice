0c9c06c026a8a0d0276c12a72e29f6d2786c057f
lovjesus2 <39009566+lovjesus2@users.noreply.github.com>
2025-07-28 16:58:21 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-import { skeleton } from '@skeletonlabs/skeleton/tailwind/skeleton.cjs';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './node_modules/@skeletonlabs/skeleton/**/*.{html,js,svelte,ts}'
+    './src/**/*.{html,js,svelte,ts}'
   ],
   theme: {
     extend: {
@@ -16,10 +13,5 @@ export default {
       }
     },
   },
-  plugins: [
-    skeleton({
-      themes: { preset: [ "skeleton" ] }
-    }),
-    require('@tailwindcss/forms')
-  ],
+  plugins: [],
 }
